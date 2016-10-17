44cd99b0c5d89b3ec219839888de9913ed4247d0
csirirathasuk14 <csirirathasuk14@gmail.com>
2016-10-17 11:59:28 -0400

    was able to added photos to details page

--- Wardrobe1/Models/Model1.Designer.cs
+++ Wardrobe1/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\wecancodeit\documents\visual studio 2015\Projects\Wardrobe1\Wardrobe1\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\WeCanCodeIT\documents\visual studio 2015\Projects\Wardrobe1\Wardrobe1\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
